645007cb0a32a1f1cfb823104ada6908391cf768
sandunrmst <sandunalugc@gmail.com>
2023-09-02 14:38:36 +0530

    Create user login and admin login

---
+++ src/Store/ReduxSlice/userSlice.js
@@ -0,0 +1,27 @@
+
+import { createSelector, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user:{
+        name: "sandun",
+    },
+}
+
+const userSlice = createSlice({
+    name:'userSlice',
+    initialState,
+    reducers:{
+        addUsers: (state, action) => {
+            state.user = action.payload
+        },
+        removeUser: (state, action)=>{
+            state.user = {}
+        }
+    }
+})
+
+export const {addUsers, removeUser} = userSlice.actions
+
+export const userSelector = createSelector([(store)=> store.user.user], (user)=>user)
+
+export default userSlice.reducer
